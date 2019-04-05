9e140a564bae1056747b18686b6438151979e419
ammai.shankar <ammai.shankar@gmail.com>
2019-04-05 16:52:14 +0100

    Basic Design

---
+++ Tic Tac Toe/Tic Tac Toe/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Tic_Tac_Toe.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Tic Tac Toe")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Tic Tac Toe")>]
+[<assembly: AssemblyCopyright("Copyright ©  2019")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("ef5f0a30-a2a5-4e39-9adf-0df34774eb02")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//       Major Version
+//       Minor Version
+//       Build Number
+//       Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Tic Tac Toe/Tic Tac Toe/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+open FsXaml
+type Game=XAML<"TicTacToe.xaml">
+[<EntryPoint;STAThread>]
+let main argv = 
+    Game().Run()
+    
